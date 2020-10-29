db679bb5c51377845296df7d770c01b88dd229c3
francesca-giorno <francescagiorno1996@gmail.com>
2020-10-29 18:09:26 +0100

    parte senza bonus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,71 @@
-//genero numeri random
+ /*//genero numeri random
 function randomNum( min, max){
   var numeroRandom = Math.floor(Math.random()* (max-min+1) + min);
   return numeroRandom;
 }
+//array
+var pcListNmber[]
+
+while (pcListNmber.lenght < 16) {
+  var newRandomNum = numeroRandom(1, 100);
+
+
+
+}*/
+var min = 1;
+var max = 100;
+var round = 5; //per ora poi 84
+
+ //definisco Array
+ var pcListNumber [];
+
+ // genero numero randomico
+ while ( pcListNumber < 16;){
+   var numeroRandom = Math.floor(Math.random()* (max - min + 1)+min);
+   if( !numArray(numeroRandom, pcListNumber)){
+      pcListNumber.push(numeroRandom);
+   }
+
+ }
+//funzione che sta nella condizione dell'if e che se è vera genera un elemento mentre se è falsa non lo genera
+function numArray(number, numberArr){
+  for(var i= 0; i < numberArr.lenght; i++){
+    if(number === numberArr[i]){
+      return true;
+    }
+  } return false;
+}
+
+//blocco centrale
+//condizioni per continuare il gioco
+ var vinto =false;
+ var isAlive = true;
+ var sceltaUtenteArr [];
+
+ // ciclo while
+ while (!vinto && isALive){
+   var sceltaUtente = parseInt(prompt("Devi scegliere un numero fra" + min "e" + max));
+       if(numArray(sceltaUtente, sceltaUtenteArr)){
+         alert.("Numero già inserito, riteta, sarai più fortunato!");
+       } else{
+         sceltaUtenteArr.push(sceltaUtente);
+         if(numArray(sceltaUtente, pcListNumber)){
+           isAlive = false;
+         } else if (sceltaUtenteArr.lenght == round){
+           vinto = true;
+         }
+       }
+ }
+
+ //stampa utente vince o perde
+ var messageEL = document.getElementById("messageOne");
+
+ if(vinto){
+  .innerHTML ="Hai vinto!!!";
+
+} else{
+  innerHTML = "Hai perso!!!";
+}
+
+// stampa numero tentativi
+var tentativiEL = document.getElementById("tentativi").innerHTML += sceltaUtenteArr.lenght
